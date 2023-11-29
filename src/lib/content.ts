2b590c63df9aeca609a5dbb32f2fb5d1cf6ac83c
Rajat Gupta <7rgdev@gmail.com>
2023-11-29 15:14:04 +0530

    fetch the content and then parse the content into sentences

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -3,12 +3,25 @@ const API_URL = "http://localhost:5174/content";
 /**
  * Fetch the content from the api
  */
-const fetchContent = async () => {};
+const fetchContent = async () => {
+  //get the content
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  return data;
+};
 
 /**
  * Parse the content into sentences, and return an array of sentences. Look at the Readme for sample input and expected output.
  * Avoid using DOMParser for implementing this function.
  */
-const parseContentIntoSentences = (content: string) => {};
+const parseContentIntoSentences = (content: string): string[] => {
+  return (
+    content
+      .match(/<s>(.*?)<\/s>/g)
+      ?.map((c) =>
+        c.replace(/<\/?s>/g, "").concat(c.includes(".") ? " " : ".")
+      ) ?? []
+  );
+};
 
 export { fetchContent, parseContentIntoSentences };
