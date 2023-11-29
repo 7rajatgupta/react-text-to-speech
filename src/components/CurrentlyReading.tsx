528303cd8876432c8d3ddaa7a9f18195e0426ac6
Rajat Gupta <7rgdev@gmail.com>
2023-11-29 15:23:56 +0530

    CurrentlyReading component structure

--- src/components/CurrentlyReading.tsx
+++ src/components/CurrentlyReading.tsx
@@ -1,8 +1,34 @@
-// Implement a component that displays the currently read word and sentence 
-export const CurrentlyReading = ({}) => {
+// Implement a component that displays the currently read word and sentence
+
+interface CurrentlyReadingProps {
+  word: string;
+  sentence: string;
+  allSentences: string[];
+}
+export const CurrentlyReading = ({
+  word,
+  sentence,
+  allSentences,
+}: CurrentlyReadingProps) => {
   return (
     <div className="currently-reading">
-
+      <div>
+        <h3>Currently Reading :</h3>
+        <div>
+          {sentence?.split(" ").map((currentWord: string, id: number) => {
+            const isHighlightedWord = currentWord === word;
+            return (
+              <span
+                key={id}
+                className={isHighlightedWord ? "highlighed-word" : ""}
+              >
+                {currentWord}
+              </span>
+            );
+          })}
+        </div>
+      </div>
+      <div>All Sentences : {allSentences.join(" ")}</div>
     </div>
   );
 };
