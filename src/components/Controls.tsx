874f8f0981f77cc9e1abffaf5a162d7b02bf000f
Rajat Gupta <7rgdev@gmail.com>
2023-11-29 16:06:00 +0530

    Control Component Structure

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,4 +1,37 @@
+import { Fragment } from "react";
+import { PlayingState } from "../lib/speech";
+import { FaPlay, FaPause } from "react-icons/fa";
+import { MdCancel } from "react-icons/md";
 // Implement a component that provides basic UI options such as playing, pausing and loading new content
-export const Controls = () => {
-  return <div></div>;
+
+interface ControlProps {
+  state: PlayingState;
+  controls: {
+    play: () => void;
+    pause: () => void;
+    cancel: () => void;
+  };
+}
+export const Controls = ({
+  controls: { play, pause, cancel },
+  state,
+}: ControlProps) => {
+  return (
+    <Fragment>
+      <div>
+        <h2>Controls</h2>
+      </div>
+      <div className="control-buttons">
+        <button onClick={play}>
+          <FaPlay />
+        </button>
+        <button onClick={pause}>
+          <FaPause />
+        </button>
+        <button onClick={cancel}>
+          <MdCancel />
+        </button>
+      </div>
+    </Fragment>
+  );
 };
