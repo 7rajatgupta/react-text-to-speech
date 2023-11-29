33bffba9e15c1669906c31953710d2aac5903efa
Rajat Gupta <7rgdev@gmail.com>
2023-11-29 16:07:00 +0530

    App.tsx changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,38 @@
-import './App.css';
-
-import { Controls } from './components/Controls';
-import { CurrentlyReading } from './components/CurrentlyReading';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { useSpeech } from "./lib/useSpeech";
+import { Controls } from "./components/Controls";
+import { CurrentlyReading } from "./components/CurrentlyReading";
+import { fetchContent, parseContentIntoSentences } from "./lib/content";
 
 function App() {
-  // const [sentences, setSentences] = useState<Array<string>>([]);
-  // const { currentWord, currentSentence, controls } = useSpeech(sentences);
+  const [sentences, setSentences] = useState<Array<string>>([]);
+  const { currentWord, activeSentence, controls, playingState } =
+    useSpeech(sentences);
 
+  useEffect(() => {
+    fetchContent()
+      .then((content) => {
+        const parsed = parseContentIntoSentences(content);
+        setSentences(parsed);
+      })
+      .catch((error) => console.error(error));
+  }, []);
   return (
-    <div className="App">
+    <>
+      <header className="header">Text to Speech</header>
       <h1>Text to speech</h1>
-      <div>
-        <CurrentlyReading/>
-      </div>
-      <div>
-        <Controls/>
-      </div>
-    </div>
+      <section className="currently-reading">
+        <CurrentlyReading
+          word={currentWord}
+          sentence={activeSentence}
+          allSentences={sentences}
+        />
+      </section>
+      <section className="controls">
+        <Controls controls={controls} state={playingState} />
+      </section>
+    </>
   );
 }
 
